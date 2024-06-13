c83e06ec76d26de7d95f18d10fe4622835d0c272
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-06-13 21:13:19 +0200

    chore: new Java SpringBoot project initialized for Pet Clinic backend
    Java compatibility: version 21 and above

---
+++ petClinicBackend/src/main/java/com/greenfox/dramacsoport/petclinicbackend/PetClinicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.dramacsoport.petclinicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicBackendApplication.class, args);
+    }
+
+}
