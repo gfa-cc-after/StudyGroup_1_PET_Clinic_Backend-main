e10930ef9a0904ac2ad87836974a01ab980385a6
Agnes Sulyok <Agnes.Mm.Molnar@gmail.com>
2024-06-29 13:59:22 +0200

    added backend-ci.yaml and a dummy test

--- src/test/java/com/greenfox/dramacsoport/petclinicbackend/PetClinicBackendApplicationTests.java
+++ src/test/java/com/greenfox/dramacsoport/petclinicbackend/PetClinicBackendApplicationTests.java
@@ -1,5 +1,6 @@
 package com.greenfox.dramacsoport.petclinicbackend;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,12 @@ class PetClinicBackendApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void dummyTest(){
+        int x = 1;
+        Assertions.assertEquals(1,x);
     }
 
 }
