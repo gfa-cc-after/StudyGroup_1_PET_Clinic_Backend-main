29ff4702b5453926bd1a28692b88c4a9d0a5c2ee
Rew <illes.rew.anna@gmail.com>
2024-07-12 19:36:14 +0200

    Start to work on sending mail in MyUserService

--- src/main/java/com/greenfox/dramacsoport/petclinicbackend/services/MyUserService.java
+++ src/main/java/com/greenfox/dramacsoport/petclinicbackend/services/MyUserService.java
@@ -2,9 +2,14 @@ package com.greenfox.dramacsoport.petclinicbackend.services;
 
 import com.greenfox.dramacsoport.petclinicbackend.models.MyUser;
 import com.greenfox.dramacsoport.petclinicbackend.repositories.MyUserRepository;
+import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.mail.SimpleMailMessage;
 
 @Service
 public class MyUserService {
@@ -13,6 +18,12 @@ public class MyUserService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String petClinicEmail;
 
     public boolean isUserRegistered (String email){
         return myUserRepository.findByEmail(email).isPresent();
@@ -23,6 +34,7 @@ public class MyUserService {
     }
 
     public MyUser saveUser(MyUser user){
+        sendEmailAfterRegistration(user);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return myUserRepository.save(user);
     }
@@ -32,4 +44,18 @@ public class MyUserService {
                 || user.getUsername() == null || user.getUsername().isEmpty()
                 || user.getPassword() == null || user.getPassword().isEmpty();
     }
+
+    private void sendEmailAfterRegistration(MyUser user){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(petClinicEmail);
+        message.setTo(user.getEmail());
+        message.setSubject("Registration successful - Pet Clinic");
+        message.setText("Dear " + user.getUsername()+ ",\n\n" +
+                "Thank you for registering to our Pet Clinic application!\n\n" +
+                "Best regards,\n" +
+                "Pet Clinic Team");
+
+        javaMailSender.send(message);
+    }
+
 }
