f2f52f47a3caa250bef953a259d635e3e8de9c58
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-07-15 20:18:01 +0200

    chore: DTOs created and moved to DTO package

---
+++ src/main/java/com/greenfox/dramacsoport/petclinicbackend/dtos/LoginForm.java
@@ -0,0 +1,4 @@
+package com.greenfox.dramacsoport.petclinicbackend.dtos;
+
+public record LoginForm (String email, String password) {
+}
---
+++ src/main/java/com/greenfox/dramacsoport/petclinicbackend/dtos/RegisterDTO.java
@@ -0,0 +1,4 @@
+package com.greenfox.dramacsoport.petclinicbackend.dtos;
+
+public record RegisterDTO(String username, String email, String password) {
+}
