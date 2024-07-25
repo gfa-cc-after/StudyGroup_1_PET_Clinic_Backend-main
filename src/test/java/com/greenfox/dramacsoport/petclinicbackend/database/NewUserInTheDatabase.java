58e7b078ed9b3ff7549483778749d70f5ca8f342
BalazsiOliver <balazsi.oliver04@gmail.com>
2024-07-25 22:36:39 +0200

    create: test class for new user in database(sticking to the ticket description)

---
+++ src/test/java/com/greenfox/dramacsoport/petclinicbackend/database/NewUserInTheDatabase.java
@@ -0,0 +1,10 @@
+package com.greenfox.dramacsoport.petclinicbackend.database;
+
+import com.greenfox.dramacsoport.petclinicbackend.services.MyUserService;
+import org.mockito.InjectMocks;
+
+public class NewUserInTheDatabase {
+
+    @InjectMocks
+    private MyUserService myUserService;
+}
