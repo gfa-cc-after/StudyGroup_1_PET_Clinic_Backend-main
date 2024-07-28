8e0d4b079169ebfa4daaad3d98fc356f24f838c8
BalazsiOliver <balazsi.oliver04@gmail.com>
2024-07-28 20:48:45 +0200

    typo:remake convertToEntity method to private

--- src/main/java/com/greenfox/dramacsoport/petclinicbackend/services/MyUserService.java
+++ src/main/java/com/greenfox/dramacsoport/petclinicbackend/services/MyUserService.java
@@ -31,7 +31,7 @@ public class MyUserService {
      * @param sourceDTO the DTO, you want to convert
      * @return MyUser entity
      */
-    public <T> MyUser convertToEntity(T sourceDTO){
+    private  <T> MyUser convertToEntity(T sourceDTO){
         return modelMapper.map(sourceDTO, MyUser.class);
     }
 
