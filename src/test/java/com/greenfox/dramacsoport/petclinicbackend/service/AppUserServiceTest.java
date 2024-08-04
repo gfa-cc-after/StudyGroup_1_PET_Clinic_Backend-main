5cbf28867d140be69d0e486b1217344bec54729d
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-08-04 21:48:04 +0200

    style and refactor: only minor changes, functionality remained unchanged
    - removed unused imports
    - moved petClinicEmail under setup() as local var. (IntelliJ recommendation)

--- src/test/java/com/greenfox/dramacsoport/petclinicbackend/service/AppUserServiceTest.java
+++ src/test/java/com/greenfox/dramacsoport/petclinicbackend/service/AppUserServiceTest.java
@@ -5,28 +5,23 @@ import com.greenfox.dramacsoport.petclinicbackend.dtos.RegisterRequestDTO;
 import com.greenfox.dramacsoport.petclinicbackend.errors.AppServiceErrors;
 import com.greenfox.dramacsoport.petclinicbackend.exeptions.PasswordException;
 import com.greenfox.dramacsoport.petclinicbackend.models.AppUser;
-
 import com.greenfox.dramacsoport.petclinicbackend.repositories.AppUserRepository;
-
 import com.greenfox.dramacsoport.petclinicbackend.services.AppUserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.naming.NameAlreadyBoundException;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class AppUserServiceTest {
@@ -48,14 +43,13 @@ public class AppUserServiceTest {
 
     private RegisterRequestDTO registerRequestDTO;
 
-    private final String petClinicEmail = "petclinic@example.com";
-
     @BeforeEach
     public void setup() {
         // Initialize test data
         registerRequestDTO = new RegisterRequestDTO("testuser", "test@example.com","password");
 
         // Set up the email sender
+        String petClinicEmail = "petclinic@example.com";
         System.setProperty("spring.mail.username", petClinicEmail);
 
         appUserService = new AppUserServiceImpl(
