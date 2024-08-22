7e5ef306617de6d79fd42b319d1ac2e8231704cd
Ravasz-Vozár Dóra <ravaszvozard@gmail.com>
2024-08-22 20:44:45 +0200

    Implement core user deletion logic.

--- src/main/java/com/greenfox/dramacsoport/petclinicbackend/controllers/MainController.java
+++ src/main/java/com/greenfox/dramacsoport/petclinicbackend/controllers/MainController.java
@@ -1,10 +1,13 @@
 package com.greenfox.dramacsoport.petclinicbackend.controllers;
 
+import com.greenfox.dramacsoport.petclinicbackend.services.appUser.AppUserService;
 import com.greenfox.dramacsoport.petclinicbackend.services.petHandling.PetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
@@ -14,6 +17,8 @@ import java.security.Principal;
 public class MainController {
 
     private final PetService petService;
+    private final AppUserService appUserService;
+
     @GetMapping("/admin/home")
     public String handleAdminHome() {
         return "home_admin";
@@ -24,4 +29,13 @@ public class MainController {
         return new ResponseEntity<>(petService.getUserPets(user.getName()), HttpStatus.OK);
     }
 
+    @DeleteMapping("/user/delete")
+    public ResponseEntity<?> deleteUser(Principal user) {
+        System.out.println(user.getName());
+        try {
+            return new ResponseEntity<>(appUserService.deleteUser(user.getName()), HttpStatus.OK);
+        } catch (UsernameNotFoundException e) {
+            return new ResponseEntity<>("Bad credentials!", HttpStatus.FORBIDDEN);
+        }
+    }
 }
---
+++ src/main/java/com/greenfox/dramacsoport/petclinicbackend/dtos/delete/DeleteUserResponse.java
@@ -0,0 +1,4 @@
+package com.greenfox.dramacsoport.petclinicbackend.dtos.delete;
+
+public record DeleteUserResponse(String message) {
+}
