7f44b4e354403922d3689febda34955d378ef5f7
Ravasz-Vozár Dóra <ravaszvozard@gmail.com>
2024-08-24 09:28:15 +0200

    Delete unused catch

--- src/main/java/com/greenfox/dramacsoport/petclinicbackend/controllers/appUser/AppUserController.java
+++ src/main/java/com/greenfox/dramacsoport/petclinicbackend/controllers/appUser/AppUserController.java
@@ -1,6 +1,5 @@
 package com.greenfox.dramacsoport.petclinicbackend.controllers.appUser;
 
-import com.greenfox.dramacsoport.petclinicbackend.controllers.appUser.auth.RegisterController;
 import com.greenfox.dramacsoport.petclinicbackend.exeptions.DeletionException;
 import com.greenfox.dramacsoport.petclinicbackend.services.appUser.AppUserService;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
@@ -19,16 +17,13 @@ import java.security.Principal;
 public class AppUserController {
 
     private final AppUserService appUserService;
-    private final Logger logger = LoggerFactory.getLogger(RegisterController.class);
+    private final Logger logger = LoggerFactory.getLogger(AppUserController.class);
 
     @DeleteMapping("/user/delete")
     public ResponseEntity<?> deleteUser(Principal user) {
         try {
             logger.info("Deleting user: {}", user.getName());
             return new ResponseEntity<>(appUserService.deleteUser(user.getName()), HttpStatus.OK);
-        } catch (UsernameNotFoundException e) {
-            logger.error("Bad credentials", e);
-            return new ResponseEntity<>("Bad credentials!", HttpStatus.FORBIDDEN);
         } catch (DeletionException e) {
             logger.error("Error while deleting user: {}", user.getName(), e);
             return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
