6b90e5fcb4628512919ab37a7d752b5a61784cc3
András Földesi <146826863+bsnAndras@users.noreply.github.com>
2024-09-05 21:29:45 +0200

    test: null checks in Change User Data controller test
    - Happy path with null password field
    - Unhappy with ALL fields null

--- src/test/java/com/greenfox/dramacsoport/petclinicbackend/controllers/appUser/AppUserControllerTest.java
+++ src/test/java/com/greenfox/dramacsoport/petclinicbackend/controllers/appUser/AppUserControllerTest.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.greenfox.dramacsoport.petclinicbackend.dtos.delete.DeleteUserResponse;
 import com.greenfox.dramacsoport.petclinicbackend.dtos.update.EditUserRequestDTO;
 import com.greenfox.dramacsoport.petclinicbackend.dtos.update.EditUserResponseDTO;
+import com.greenfox.dramacsoport.petclinicbackend.exceptions.IncorrectPasswordException;
 import com.greenfox.dramacsoport.petclinicbackend.exceptions.UnauthorizedActionException;
 import com.greenfox.dramacsoport.petclinicbackend.services.appUser.AppUserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.HashMap;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -55,18 +60,19 @@ public class AppUserControllerTest {
         // Assert
         verify(appUserService, times(1)).deleteUser("testuser", 1L);
     }
-    
+
     @Test
     @WithMockUser("testuser@test.com")
     @DisplayName("Delete user Controller - UNHAPPY PATH (HTTP 403 FORBIDDEN)")
     public void deleteUser_shouldReturn403Forbidden_whenUserIsAuthenticatedAndIdIsNotMatching() throws Exception {
         // Arrange
-        when(appUserService.deleteUser("testuser@test.com", 2L)).thenThrow(new UnauthorizedActionException("User is not authorized to delete this account"));
+        when(appUserService.deleteUser("testuser@test.com", 2L)).thenThrow(new UnauthorizedActionException("User is " +
+                "not authorized to delete this account"));
 
         // Act & Assert
         this.mockMvc.perform(delete("/api/v1/user/2")
-                            .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(status().isForbidden());
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
     }
 
     @Test
@@ -94,5 +100,66 @@ public class AppUserControllerTest {
                         content().json("{\"message\":\"Changes saved.\"}")
                 );
         verify(appUserService).changeUserData(anyString(), any(EditUserRequestDTO.class));
+    }
+
+    @Test
+    @WithMockUser("testUser")
+    @DisplayName("Edit user Controller - HAPPY PATH with PW null (HTTP 200 OK)")
+    public void editUser_shouldReturn200Ok_whenUserIsAuthenticatedAndDataIsCorrectNullPW() throws Exception {
+        // Arrange
+        String userEmail = "testUser";
+        EditUserRequestDTO requestDTO = new EditUserRequestDTO(
+                "new@example.com",
+                "0ld_P4ssw0rd",
+                null,
+                "New_Name"
+        );
+        EditUserResponseDTO responseDTO = new EditUserResponseDTO("Changes saved.");
+
+        when(appUserService.changeUserData(userEmail, requestDTO)).thenReturn(responseDTO);
+
+        // Act & Assert
+        this.mockMvc.perform(post("/api/v1/user/profile")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDTO)))
+                .andExpectAll(
+                        status().isOk(),
+                        content().json("{\"message\":\"Changes saved.\"}")
+                );
+        verify(appUserService).changeUserData(anyString(), any(EditUserRequestDTO.class));
+    }
+
+    @Test
+    @WithMockUser("testUser")
+    @DisplayName("Edit user Controller - UNHAPPY PATH null fields (HTTP BAD)")
+    public void editUser_shouldThrowValidationErrorsWhenFieldsAreNull() throws Exception {
+        // Arrange
+        String userEmail = "testUser";
+        EditUserRequestDTO requestDTO = new EditUserRequestDTO(
+                null,
+                null,
+                null,
+                null
+        );
+
+        when(appUserService.changeUserData(userEmail, requestDTO)).thenThrow(new IncorrectPasswordException());
+
+        // Act & Assert
+        MvcResult result = mockMvc.perform(post("/api/v1/user/profile")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDTO)))
+                .andExpect(
+                        status().isBadRequest())
+                .andReturn();
+
+        verify(appUserService, never()).changeUserData(anyString(), any(EditUserRequestDTO.class));
+        HashMap<String, String> errorResponse = objectMapper.readValue(result.getResponse().getContentAsString(),
+                HashMap.class);
+        Assertions.assertEquals("Please enter a valid email address!", errorResponse.get("email"));
+        Assertions.assertEquals("Please enter your original password!", errorResponse.get("originalPassword"));
+        Assertions.assertNull(errorResponse.get("password"));
+        Assertions.assertEquals("Please enter your display name!", errorResponse.get("displayName"));
+
+
     }
 }
