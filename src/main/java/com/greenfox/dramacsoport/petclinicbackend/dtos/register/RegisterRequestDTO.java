981e512580b4e52d9f1a7057ad2df9699753711e
András Földesi <146826863+bsnAndras@users.noreply.github.com>
2024-09-08 19:29:24 +0200

    patch: DTOs unneeded validations got removed (@NotNull is not needed, where @NotBlank or @NotEmpty is in place)

--- src/main/java/com/greenfox/dramacsoport/petclinicbackend/dtos/register/RegisterRequestDTO.java
+++ src/main/java/com/greenfox/dramacsoport/petclinicbackend/dtos/register/RegisterRequestDTO.java
@@ -4,23 +4,19 @@ import com.greenfox.dramacsoport.petclinicbackend.errors.AppServiceErrors;
 import com.greenfox.dramacsoport.petclinicbackend.models.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public final class RegisterRequestDTO {
     @NotBlank(message = "Display name field is required.")
-    @NotNull(message = "Display name is required.")
     private final String displayName;
 
     @NotBlank(message = "Email field is required.")
-    @NotNull(message = "Email field is required.")
     @Email(message = AppServiceErrors.EMAIL_FIELD_NOT_VALID)
     private final String email;
 
     @NotBlank(message = "Password field is required.")
-    @NotNull(message = "Password field is required.")
     @Size(min = 3, message = AppServiceErrors.SHORT_PASSWORD)
     private final String password;
 
